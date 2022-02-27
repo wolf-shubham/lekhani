b79dd5620eaf776adccd500dea9b082befa21d26
Shubham Sharma <sharma.shubham0191@gmail.com>
2022-02-27 23:20:22 +0530

    database initialized

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
 
 const app = express()
+dotenv.config()
+const PORT = process.env.PORT || 5000
 
-app.listen('5000', (req, res) => {
-    console.log(`server started`)
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(console.log('mongoDB connected'))
+    .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+    console.log(`server started at ${PORT}`)
 })
