b260792dfd91d3f7f88785fa700e5167a065547d
Shubham Sharma <sharma.shubham0191@gmail.com>
2022-03-12 22:52:53 +0530

    code refactoring

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -20,7 +20,7 @@ function Register() {
         }
     }, [userData, history])
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
         dispatch(userRegisterAction(name, email, password))
     }
@@ -29,9 +29,30 @@ function Register() {
         <>
             <h1>register...</h1>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="name" label='name' id="name" required placeholder='name' value={name} onChange={(e) => setName(e.target.value)} />
-                <input type="email" name="email" id="email" label='email' required placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" name="password" id="password" label='password' required placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input
+                    type="text"
+                    name="name"
+                    label='name'
+                    id="name"
+                    required placeholder='name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} />
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    label='email'
+                    required placeholder='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    label='password'
+                    required placeholder='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
                 <button type="submit">Register</button>
                 <p>already have an account? <Link to='/login'>Login</Link></p>
             </form>
